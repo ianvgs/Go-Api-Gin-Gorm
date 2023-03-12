9bddd775169efe7f956698c3961ec1d274deeb9d
Ian <ian.vgs@hotmail.com>
2023-03-12 11:20:01 -0300

    feat: adding connection with mongDB

---
+++ models/mongo/user_model.go
@@ -0,0 +1,10 @@
+package mongomodels
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	Id       primitive.ObjectID `bson:"id,omitempty"`
+	Name     string             `json:"name,omitempty" validate:"required"`
+	Location string             `json:"location,omitempty" validate:"required"`
+	Title    string             `json:"title,omitempty" validate:"required"`
+}
--- routes/routes.go
+++ routes/routes.go
@@ -9,7 +9,15 @@ import (
 func HandleRequests() {
 	r := gin.Default()
 	r.GET("/posts", controllers.PostIndex)
+
 	r.POST("/posts", controllers.PostCreate)
+
+	r.POST("/user", controllers.CreateUser())
+	r.GET("/user/:userId", controllers.GetAUser())
+	r.PUT("/user/:userId", controllers.EditAUser())
+	r.DELETE("/user/:userId", controllers.DeleteAUser())
+	r.GET("/users", controllers.GetAllUsers())
+
 	r.GET("/posts/:id", controllers.PostShow)
 	r.PUT("/posts/:id", controllers.PostUpdate)
 	r.PATCH("/posts/:id", controllers.PostUpdater)
