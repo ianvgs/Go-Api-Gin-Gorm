f47a725121b932a2d90a65328cd6def06705b091
Ian Guedes <ian.vgs@hotmail.com>
2023-04-03 13:22:06 -0300

    ,

--- initializers/database.go
+++ initializers/database.go
@@ -7,30 +7,39 @@ import (
 	"gorm.io/gorm"
 )
 
-const DB_USERNAME = "ian"
-const DB_PASSWORD = "1708"
-const DB_NAME = "ian"
-const DB_HOST = "127.0.0.1"
-const DB_PORT = "3306"
-
-/* // Database struct
-type Database struct {
-	DB *gorm.DB
-} */
-
 var DB *gorm.DB
+
+/* func awsURIMYSQL() string {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	DB_USERNAME := os.Getenv("DB_USERNAME")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_PORT := os.Getenv("DB_PORT")
+
+	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
+
+	return dsn
+} */
 
 func ConnectToDB() /* Database */ {
 
-	/* 	USER := os.Getenv("DB_USER")
-	PASS := os.Getenv("DB_PASSWORD")
-	HOST := os.Getenv("DB_HOST")
-	DBNAME := os.Getenv("DB_NAME") */
+	DB_USERNAME := "root"
+	DB_PASSWORD := "genuine123"
+	DB_HOST := "awsian.ct8srtp2hhm0.us-east-1.rds.amazonaws.com"
+	DB_NAME := "aws"
+	DB_PORT := "3306"
 
 	var err error
 
 	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
 
+	/* dsn := awsURIMYSQL() */
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	//no padrão ele faz db, err := cria o db e o erro ao mesmo tempo
