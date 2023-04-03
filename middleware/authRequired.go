c9c498b151b6913d248906445b98b1167a596c6e
Ian Guedes <ian.vgs@hotmail.com>
2023-04-03 11:16:48 -0300

    ,

--- middleware/authRequired.go
+++ middleware/authRequired.go
@@ -9,23 +9,13 @@ import (
 	"net/http"
 )
 
-/* The middleware for checking if a user has been authenticated is implemented in this file. It simply checks to see if the session exists. If the session is nil, the user is redirected to the /login route. Otherwise, the Gin framework is told to move to the next step. */
-
 func AuthRequired(c *gin.Context) {
 	session := sessions.Default(c)
 	user := session.Get(globals.UserKey)
-	if user == nil {
+	if user == nil || user == "" {
 		log.Println("User not logged in: user:", user)
 		c.Redirect(http.StatusMovedPermanently, "/login")
 		c.Abort()
 		return
 	}
-
-	if user == "" {
-		log.Println("User not logged in: user:", user)
-		c.Redirect(http.StatusMovedPermanently, "/login")
-		c.Abort()
-		return
-	}
-	c.Next()
 }
