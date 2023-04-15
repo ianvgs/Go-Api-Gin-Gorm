ad007d99fdf8c90203bb7c7c9f5db1feab83d655
Ian <ian.vgs@hotmail.com>
2023-04-15 01:26:21 -0300

    feat: separando as variaveis no .env e colocando .exe no gitignore

--- initializers/database.go
+++ initializers/database.go
@@ -2,56 +2,48 @@ package initializers
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-/* func awsURIMYSQL() string {
+func envMySqlString() string {
 
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	log.Println("Error:", err)
+
 	DB_USERNAME := os.Getenv("DB_USERNAME")
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
-	DB_HOST := os.Getenv("DB_HOST")
 	DB_NAME := os.Getenv("DB_NAME")
+	DB_HOST := os.Getenv("DB_HOST")
 	DB_PORT := os.Getenv("DB_PORT")
+	log.Println("Username:", DB_USERNAME)
 
 	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
 
 	return dsn
-} */
+}
 
-func ConnectToDB() /* Database */ {
-
-	/* DB_USERNAME := "root"
-	DB_PASSWORD := "genuine123"
-	DB_HOST := "awsian.ct8srtp2hhm0.us-east-1.rds.amazonaws.com"
-	DB_NAME := "aws"
-	DB_PORT := "3306" */
-
-	const DB_USERNAME = "roots"
-	const DB_PASSWORD = ""
-	const DB_NAME = "news_database"
-	const DB_HOST = "127.0.0.1"
-	const DB_PORT = "3306"
+func ConnectToDB() {
 
 	var err error
+	dsn := envMySqlString()
 
-	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
-
-	/* dsn := awsURIMYSQL() */
+	//log.Println("Dsn:", dsn)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	//Desse jeito ele não deixa a aplicação rodar
-	/* if err != nil {
+	if err != nil {
 		log.Fatal("Erro ao conectar ao banco de dados")
-	} */
+	}
 
 	if err != nil {
 		log.Println("Erro ao conectar ao banco de dados:", err)
