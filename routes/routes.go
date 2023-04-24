030c94640f90f68f7adb0242a4456078e94fae3f
Ian <ian.vgs@hotmail.com>
2023-04-24 20:57:45 -0300

    .

--- routes/routes.go
+++ routes/routes.go
@@ -57,8 +57,7 @@ func HandleRequests() {
 			log.Fatalf("Error getting executable path: %s", err)
 		}
 		executableDir := filepath.Dir(executablePath)
-		r.Static("/js", filepath.Join(executableDir, "js"))
-		r.Static("/assets", filepath.Join(executableDir, "assets"))
+		r.Static("/assets", filepath.Join(executableDir, "./assets"))
 		r.LoadHTMLGlob(filepath.Join(executableDir, "templates/*.html"))
 
 	}
@@ -69,8 +68,8 @@ func HandleRequests() {
 		r.Use(gin.Logger())
 
 		//Configs
-		r.Static("/js", "./js")
 		r.Static("/assets", "./assets")
+		r.Static("/js", "./js")
 		r.LoadHTMLGlob("templates/*.html")
 	}
 	r.Run()
